c25743f364a0c392cdc6cf5548cc902d97bc11a2
Ryukura <ryoukora@gmail.com>
2025-05-18 12:06:18 +0700

    Update 1-minute-script.sh

--- Router/scripts/1-minute-script.sh
+++ Router/scripts/1-minute-script.sh
@@ -30,19 +30,16 @@ publicip="${public_ipv4},${public_ipv6}"
 echo "wanip=${wanip}" "publicip=${publicip}" > /tmp/wanip.out
 
 sleep 1
-
 #---------------------------------------------------------------------------------------------------------#
 # Run nlbw export to CSV and save to file
 if command -v nlbw >/dev/null; then
     nlbw -c csv -g ip,mac -o ip | tr -d '"' | tail -n +2 > /tmp/nlbwmon.out
 fi
-
 #---------------------------------------------------------------------------------------------------------#
 # Check if internet-outage.sh is running, if not, start it
 if ! pgrep -f "internet-outage.sh" >/dev/null; then
     /usr/bin/internet-outage.sh &
 fi
-
 #---------------------------------------------------------------------------------------------------------#
 # Run vnstat and parse output
 if command -v vnstat >/dev/null; then
@@ -52,18 +49,16 @@ if command -v vnstat >/dev/null; then
 	  #vnstat --xml |grep -hnr "hour id" | sed 's/<[^>]*>/ /g; s/2025//g; s/        //g; s/  00/:00/g' | cut -d " " -f2-  > /tmp/houroutput.out
 	  #vnstat --xml |grep -hnr "fiveminute id" | sed 's/<[^>]*>/ /g; s/2025//g; s/        //g' | cut -d " " -f2-   > /tmp/fiveoutput.out
 fi
-
 #---------------------------------------------------------------------------------------------------------#
 # Restart Netify if service is not running or using high memory
-if ! pgrep netifyd >/dev/null; then
-    /etc/init.d/netifyd start
-else
-    # Restart Netify if memory usage exceeds 25%
-    netify_mem=$(top -b -n 1 | awk '/netify/ && !/grep/ {print $6}' | tr -d '%')
-    if [ -n "$netify_mem" ] && [ "$netify_mem" -gt 25 ]; then
-        echo "Restarting Netify due to high memory usage"
-        /etc/init.d/netifyd restart
-    fi
-fi
-
+#if ! pgrep netifyd >/dev/null; then
+#    /etc/init.d/netifyd start
+#else
+#    # Restart Netify if memory usage exceeds 25%
+##    netify_mem=$(top -b -n 1 | awk '/netify/ && !/grep/ {print $6}' | tr -d '%')
+#    if [ -n "$netify_mem" ] && [ "$netify_mem" -gt 25 ]; then
+#        echo "Restarting Netify due to high memory usage"
+#        /etc/init.d/netifyd restart
+#    fi
+#fi
 #---------------------------------------------------------------------------------------------------------#
