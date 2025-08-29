cc698253e642fad2afc19cba066b85248165d9fe
Ryukura <ryoukora@gmail.com>
2025-08-29 23:29:03 +0700

    Refactor NAT traffic scraping logic and documentation
    Refactor NAT traffic scraping to use a two-dimensional table for summing bytes between source and destination. Update documentation and variable initialization for clarity.

--- Router/lua/nat_traffic.lua
+++ Router/lua/nat_traffic.lua
@@ -1,38 +1,50 @@
 local function scrape()
-  -- documetation about nf_conntrack:
+  -- documentation about nf_conntrack:
   -- https://www.frozentux.net/iptables-tutorial/chunkyhtml/x1309.html
-  nat_metric = metric("node_nat_traffic", "gauge")
 
-  local count = 0  -- Counter for limiting entries
-  local limit = 200  -- Set your desired limit
+  -- two-dimensional table to sum bytes for the pair (src/dest)
+  local nat = {}
+  -- default constructor to initialize unknown pairs
+  setmetatable(nat, {
+    __index = function (t, addr)
+      t[addr] = {}
+      setmetatable(t[addr], {
+        __index = function () return 0 end
+      })
+      return t[addr]
+    end
+  })
 
   for e in io.lines("/proc/net/nf_conntrack") do
-    if count >= limit then
-      break  -- Stop processing once the limit is reached
-    end
-
+    -- output(string.format("%s\n",e  ))
     local fields = space_split(e)
-    local src, sport, dest, dport, bytes
-    bytes = 0
+    local src, dest, bytes;
+    bytes = 0;
     for _, field in ipairs(fields) do
       if src == nil and string.match(field, '^src') then
-        src = string.match(field, "src=([^ ]+)")
-      elseif sport == nil and string.match(field, '^sport') then
-        sport = string.match(field, "sport=([^ ]+)")
+        src = string.match(field,"src=([^ ]+)");
       elseif dest == nil and string.match(field, '^dst') then
-        dest = string.match(field, "dst=([^ ]+)")
-      elseif dport == nil and string.match(field, '^dport') then
-        dport = string.match(field, "dport=([^ ]+)")
+        dest = string.match(field,"dst=([^ ]+)");
       elseif string.match(field, '^bytes') then
-        local b = string.match(field, "bytes=([^ ]+)")
-        bytes = bytes + b
+        local b = string.match(field, "bytes=([^ ]+)");
+        bytes = bytes + b;
+        -- output(string.format("\t%d %s",ii,field  ));
       end
-    end
-    
-    local labels = { src = src, sport = sport, dest = dest, dport = dport }
-    nat_metric(labels, bytes)
 
-    count = count + 1  -- Increment the counter
+    end
+    -- local src, dest, bytes = string.match(natstat[i], "src=([^ ]+) dst=([^ ]+) .- bytes=([^ ]+)");
+    -- local src, dest, bytes = string.match(natstat[i], "src=([^ ]+) dst=([^ ]+) sport=[^ ]+ dport=[^ ]+ packets=[^ ]+ bytes=([^ ]+)")
+
+    -- output(string.format("src=|%s| dest=|%s| bytes=|%s|", src, dest, bytes  ))
+    nat[src][dest] = nat[src][dest] + bytes
+  end
+
+  nat_metric =  metric("node_nat_traffic", "gauge" )
+  for src, values in next, nat do
+    for dest, bytes in next, values do
+      local labels = { src = src, dest = dest }
+      nat_metric(labels, bytes )
+    end
   end
 end
 
